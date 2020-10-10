1bca68d573c3c47794faf7cd41d8517ac836d7f3
Emmanuel Lucio Urbina <emmanuelluur@outlook.com>
2020-10-10 01:00:53 -0500

    Proyecto1 + operaciones pendientes

--- proyecto_1/main.py
+++ proyecto_1/main.py
@@ -1,16 +1,19 @@
-# Calculadora basica
 
+"""
+Calculadora basica
+Operaciones basicas (+ - * /)
+"""
 menu = """
-Menu Principal
+Menú Principal
 
 1) Suma
 2) Resta
-3) Multiplicacion
-4) Division
+3) Multiplicación
+4) División
 x) Salir
 
 """
-opt = True
+continue_program = True
 
 
 if __name__ == "__main__":
@@ -18,20 +21,58 @@ if __name__ == "__main__":
     op_1 = 0
     op_2 = 0
     res = 0
-    while opt:
-        item = input("Escribe una opcion del menu: ")
+    while continue_program:
 
-        if item == "1":
+        option = input("Escribe una opción del menú: ")
+
+        if option == "1":
             try:
                 print("Suma")
-                op_1 = float(input("Primer numero: "))
-                op_2 = float(input("Segundo numero: "))
+                op_1 = float(input("Primer número: "))
+                op_2 = float(input("Segundo número: "))
             except:
                 print("Solo son validos enteros y decimales")
             finally:
                 res = op_1 + op_2
                 print("El resultado es: {}".format(res))
+        
+        if option == "2":
+            try:
+                print("Resta")
+                op_1 = float(input("Primer número: "))
+                op_2 = float(input("Segundo número: "))
+            except:
+                print("Solo son validos enteros y decimales")
+            finally:
+                res = op_1 - op_2
+                print("El resultado es: {}".format(res))
+        
+        if option == "3":
+            try:
+                print("Multiplicación")
+                op_1 = float(input("Primer número: "))
+                op_2 = float(input("Segundo número: "))
+            except:
+                print("Solo son validos enteros y decimales")
+            finally:
+                res = op_1 * op_2
+                print("El resultado es: {}".format(res))
+        
+        if option == "4":
+            try:
+                print("Resta")
+                op_1 = float(input("Primer numero: "))
+                op_2 = float(input("Segundo numero: "))
+            except:
+                print("Solo son validos enteros y decimales o no se puede dividir entre 0")
+            finally:
+                try:
+                    res = round(op_1 / op_2,4)
+                except ZeroDivisionError as err:
+                    print("No se puede dividir entre 0")
+                finally:
+                    print("El resultado es: {}".format(res))
 
-        if item == "x" or item == "X":
-            opt = False
+        if option == "x" or option == "X":
+            continue_program = False
             print("Hasta luego...")
