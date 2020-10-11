1882174898959a0e0a074781959f3e23c6c74c7d
Emmanuel Lucio Urbina <emmanuelluur@outlook.com>
2020-10-11 12:36:04 -0500

    proyecto_3 - metodo crear libro

--- proyecto_3/main.py
+++ proyecto_3/main.py
@@ -4,9 +4,14 @@ Proyecto biblioteca
 """
 
 from models.Author import Author
+from models.Book import Book
+
 
 if __name__ == "__main__":
+    book = Book()
     author = Author()
     author.create_author("Emmanuel", "Lucio Urbina")
 
+    book.create_book("IT", "123123213", author)
     print(author)
+    print(book)
--- proyecto_3/models/Book.py
+++ proyecto_3/models/Book.py
@@ -2,7 +2,16 @@
 Modelo Libro
 """
 
+
 class Book():
     name = None
     isbn = None
     author = ""
+
+    def create_book(self, name, isbn, author):
+        self.name = name
+        self.isbn = isbn
+        self.author = author
+
+    def __str__(self):
+        return f"Libro: {self.name}\nAutor: {self.author}\nISBN: {self.isbn}"
